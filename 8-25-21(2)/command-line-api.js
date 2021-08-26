f656a1f3e2e801b7a3570f757c4390e6666e7940
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-25 21:50:13 -0600

    commit on main

---
+++ 8-25-21(2)/command-line-api.js
@@ -0,0 +1,8 @@
+// Command line API
+$('.image-item').first();
+// Searching for a class on a webpage with details for the first element (# for id and . for class)
+$('.image-item').first().hide();
+// Hides the element
+
+//Vanilla JS
+document.getElementsByClassName('image-item')[0].style.display = 'none';
